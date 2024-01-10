6ad53d6c0730e7562d2841a475c30f50d0dfd66f
ledu89 <dninkov89@gmail.com>
2024-01-10 14:44:10 +0100

    added download CV

--- src/pages/Home/Navbar.jsx
+++ src/pages/Home/Navbar.jsx
@@ -71,10 +71,10 @@ function Navbar() {
               smooth={true}
               offset={-70}
               duration={500}
-              to="MyPortfolio"
+              to="AboutMe"
               className="navbar--content"
             >
-              Portfolio
+              About Me
             </Link>
           </li>
           <li>
@@ -85,10 +85,10 @@ function Navbar() {
               smooth={true}
               offset={-70}
               duration={500}
-              to="AboutMe"
+              to="MyPortfolio"
               className="navbar--content"
             >
-              About Me
+              Portfolio
             </Link>
           </li>
           <li>
